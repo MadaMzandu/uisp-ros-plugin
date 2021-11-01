b8f64d666c4fe0b33cde6660ae3a115f5d252847
Madalitso Mzandu <mada@datacom.mw>
2021-11-01 08:09:06 +0200

    corrected database fix

--- src/public.php
+++ src/public.php
@@ -6,22 +6,30 @@ header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 chdir(__DIR__);
 
-include_once 'includes/updates.php';
-
-if(!db_is_ok()){ //restore db with defaults
-    if(!create_db()){exit();}
+if(!file_exists('data/data.db')){ //check db
+    $db = new SQLite3('data/data.db');
+    $schema = file_get_contents('includes/schema.sql') ?? null;
+    $done = false;
+    if ($db->exec($schema)) {
+        $default_conf = file_get_contents('includes/conf.sql');
+        $done = $db->exec($default_conf);
+    }
+    if(!$done){exit();}
 }
+// valid db required to proceed
+
+include_once 'includes/updates.php'; 
 
 if(!version_is_ok()){ //apply updates
     apply_updates();
 }
+
 if(!bak_is_ok()){ // create automatic backup
     create_backup();
 }
 
 include_once('lib/app_router.php');
 require_once 'vendor/autoload.php';
-
 
 
 $json = file_get_contents('php://input') ?? false;
@@ -40,6 +48,7 @@ if (isset($_SERVER['REQUEST_METHOD'])
 }
 
 if (isset($_GET['page']) && $_GET['page'] == 'panel') { //config page
-    header('Location: public/panel/index.html');
+    // header('Location: public/panel/index.html');
+    include 'public/panel/index.html';
     exit();
 } 
