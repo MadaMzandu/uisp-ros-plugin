26ce9a14760807ea59a743ebf84ba604d98892cb
Madalitso Mzandu <mada@datacom.mw>
2021-11-01 08:09:36 +0200

    set base version to 1.8.0

--- src/includes/conf.sql
+++ src/includes/conf.sql
@@ -20,6 +20,6 @@ INSERT INTO config VALUES('pppoe_pass_attr','pppoePassword',NULL,'7/17/2021 8:07
 INSERT INTO config VALUES('device_name_attr','deviceName',NULL,'7/17/2021 8:07');
 INSERT INTO config VALUES('mac_addr_attr','dhcpMacAddress',NULL,'7/17/2021 8:07');
 INSERT INTO config VALUES('ip_addr_attr','ipAddress',NULL,'7/17/2021 8:07');
-INSERT INTO config VALUES('version','1.8.1',NULL,'2021-10-30 21:42:09');
+INSERT INTO config VALUES('version','1.8.0',NULL,'2021-10-30 21:42:09');
 INSERT INTO config VALUES('disabled_rate','1',NULL,'2021-10-30 21:42:09');
 COMMIT;
