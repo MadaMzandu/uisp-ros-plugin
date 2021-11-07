7bb0b924623667b30ef5f4b2c3f2c3206f40918d
Madalitso Mzandu <mada@datacom.mw>
2021-11-07 12:32:16 +0200

    formatting

--- src/lib/mt_queue.php
+++ src/lib/mt_queue.php
@@ -4,24 +4,31 @@ class MT_Queue extends MT
 {
 
     private $pq; //parent queue object
-    private $id ;
+    private $id;
 
-    protected function init(): void
+    public function set()
     {
-        parent::init();
-        $this->path = '/queue/simple/';
-        $this->exists = $this->exists();
-        $this->pq = new MT_Parent_Queue($this->svc);
-    }
-
-    public function set(){
-        if($this->svc->contention < 0 ){
+        if ($this->svc->contention < 0) {
             return $this->delete();
         }
-        if($this->svc->contention > 0 && !$this->exists){
+        if ($this->svc->contention > 0 && !$this->exists) {
             return $this->insert();
         }
         return $this->edit();
+    }
+
+    public function delete()
+    {
+        $del['.id'] = $this->insertId;
+        if ($this->exists &&
+            !$this->write((object)$del, 'remove')) {
+            return false;
+        }
+        if (!$this->pq->set()) {  // edit or delete parent
+            $this->setErr($this->pq->error());
+            return false;
+        }
+        return true;
     }
 
     public function insert()
@@ -31,29 +38,7 @@ class MT_Queue extends MT
             return false;
         }
         $this->insertId = $this->write($this->data(), 'add');
-        return (bool) $this->insertId;
-    }
-
-    public function edit()
-    {
-        $orphanId = $this->orphaned();
-        $action = $this->exists ? 'set' : 'add';
-        $p =  $orphanId
-            ?($this->pq->reset($orphanId))
-            :($this->pq->set()) ;
-        $ret = $this->write($this->data(), $action);
-        $this->insertId = is_string($ret) ? $ret : $this->insertId;
-        return $p && (bool) $ret;
-    }
-
-    private function orphaned():string
-    {
-        if(!$this->exists()){
-            return false;
-        }
-        $queue = $this->search[0];
-        return substr($queue['parent'],0,1) == '*'
-            ? $queue['parent'] : false ;
+        return (bool)$this->insertId;
     }
 
     protected function data()
@@ -76,18 +61,34 @@ class MT_Queue extends MT
             . $this->svc->id();
     }
 
-    public function delete()
+    public function edit()
     {
-        $del['.id'] = $this->insertId;
-        if ($this->exists &&
-            !$this->write((object)$del, 'remove')) {
+        $orphanId = $this->orphaned();
+        $action = $this->exists ? 'set' : 'add';
+        $p = $orphanId
+            ? ($this->pq->reset($orphanId))
+            : ($this->pq->set());
+        $ret = $this->write($this->data(), $action);
+        $this->insertId = is_string($ret) ? $ret : $this->insertId;
+        return $p && (bool)$ret;
+    }
+
+    private function orphaned(): string
+    {
+        if (!$this->exists()) {
             return false;
         }
-        if (!$this->pq->set()) {  // edit or delete parent
-            $this->setErr($this->pq->error());
-            return false;
-        }
-        return true;
+        $queue = $this->search[0];
+        return substr($queue['parent'], 0, 1) == '*'
+            ? $queue['parent'] : false;
+    }
+
+    protected function init(): void
+    {
+        parent::init();
+        $this->path = '/queue/simple/';
+        $this->exists = $this->exists();
+        $this->pq = new MT_Parent_Queue($this->svc);
     }
 
 
