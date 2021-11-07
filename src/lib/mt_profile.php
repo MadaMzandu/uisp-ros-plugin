c6fe80e9285d48eba41acf00e999a1fcc9a1018e
Madalitso Mzandu <mada@datacom.mw>
2021-11-07 12:09:00 +0200

    removing unused code

--- src/lib/mt_profile.php
+++ src/lib/mt_profile.php
@@ -5,15 +5,7 @@ class MT_Profile extends MT
 
     private $pq; //parent queue object
 
-    protected function init():void
-    {
-        parent::init();
-        $this->path = '/ppp/profile/';
-        $this->exists = $this->exists();
-        $this->pq = new MT_Parent_Queue($this->svc);
-    }
-
-    public function set():bool
+    public function set(): bool
     {
         if ($this->svc->contention < 0 && !$this->has_children()) {
             return $this->delete();
@@ -24,56 +16,43 @@ class MT_Profile extends MT
         return $this->edit();
     }
 
-    protected function findErr(){
-        if($this->pq->status()->error){
-            $this->status = $this->pq->status();
+    private function has_children()
+    {
+        $this->path = '/ppp/secret/';
+        $read = $this->read('?profile=' . $this->name()) ?? [];
+        $disabled = $this->entity_disabled();
+        $this->path = '/ppp/profile/';
+        $count = sizeof($read) ?? 0;
+        $count += $disabled ? 0 : -1; // do not deduct if account is disabled
+        return (bool)max($count, 0);
+    }
+
+    private function entity_disabled(): bool
+    {
+        // $this->path = '/ppp/secret/'; path is already set by prev call
+        $read = $this->read('?comment');
+        $id = (string)$this->svc->id();
+        foreach ($read as $item) {
+            if (substr($item['comment'], 0, strlen($id)) == $id) {
+                return $item['profile'] == $this->conf->disabled_profile;
+            }
         }
+        return false;
     }
 
-    protected function exists():bool
+    private function delete(): bool
     {
-        $this->read('?name=' . $this->name());
-        $this->entity = $this->read[0] ?? null;
-        $this->insertId = $this->read[0]['.id'] ?? null ;
-        return (bool)$this->insertId;
-    }
-
-    protected function name()
-    {
-        return $this->svc->disabled
-            ? $this->conf->disabled_profile
-            : $this->svc->plan_name();
-    }
-
-    private function orphaned():string
-    {
-        if(!$this->exists()){
-            return false;
-        }
-        $profile = $this->entity;
-        return substr($profile['parent-queue'],0,1) == '*'
-            ? $profile['parent-queue'] : false ;
-    }
-
-    private function insert():bool
-    {
-        $this->svc->contention = +1;
-        if($this->pq->set() && $this->write($this->data(), 'add')){
-            return true ;
+        $this->svc->contention = -1;
+        $data['.id'] = $this->data()->name;
+        if ($this->pq->set()
+            && $this->write((object)$data, 'remove')) {
+            return true;
         }
         $this->findErr();
-        return false ;
+        return false;
     }
 
-    private function edit():bool
-    {
-        $orphanId = $this->orphaned();
-        return $orphanId
-            ? ($this->pq->reset($orphanId) && $this->write($this->data()))
-            : ($this->pq->set()  && $this->write($this->data()));
-    }
-
-    protected function data():object
+    protected function data(): object
     {
         return (object)[
             'name' => $this->name(),
@@ -103,7 +82,7 @@ class MT_Profile extends MT
         return false;
     }
 
-    protected function rate():object
+    protected function rate(): object
     {
         $rate = parent::rate();
         $r = $this->conf->disabled_rate;
@@ -115,41 +94,62 @@ class MT_Profile extends MT
         return $this->svc->disabled ? $disabled : $rate;
     }
 
-    private function has_children()
+    protected function findErr()
     {
-        $this->path = '/ppp/secret/';
-        $read = $this->read('?profile=' . $this->name()) ?? [];
-        $disabled = $this->entity_disabled();
-        $this->path = '/ppp/profile/';
-        $count = sizeof($read) ?? 0;
-        $count += $disabled ? 0 : -1; // do not deduct if account is disabled
-        return (bool) max($count,0);
+        if ($this->pq->status()->error) {
+            $this->status = $this->pq->status();
+        }
     }
 
-    private function entity_disabled():bool
+    private function insert(): bool
     {
-        // $this->path = '/ppp/secret/'; path is already set by prev call
-        $read = $this->read('?comment');
-        $id = (string)$this->svc->id();
-        foreach($read as $item){
-            if(substr($item['comment'],0,strlen($id)) == $id){
-                return $item['profile'] == $this->conf->disabled_profile;
-            }
+        $this->svc->contention = +1;
+        if ($this->pq->set() && $this->write($this->data(), 'add')) {
+            return true;
         }
+        $this->findErr();
         return false;
     }
 
-
-    private function delete():bool
+    private function edit(): bool
     {
-        $this->svc->contention = -1;
-        $data['.id'] = $this->data()->name;
-        if($this->pq->set()
-            && $this->write((object)$data, 'remove')){
-            return true ;
+        $orphanId = $this->orphaned();
+        return $orphanId
+            ? ($this->pq->reset($orphanId) && $this->write($this->data()))
+            : ($this->pq->set() && $this->write($this->data()));
+    }
+
+    private function orphaned(): string
+    {
+        if (!$this->exists()) {
+            return false;
         }
-        $this->findErr();
-        return false ;
+        $profile = $this->entity;
+        return substr($profile['parent-queue'], 0, 1) == '*'
+            ? $profile['parent-queue'] : false;
+    }
+
+    protected function init(): void
+    {
+        parent::init();
+        $this->path = '/ppp/profile/';
+        $this->exists = $this->exists();
+        $this->pq = new MT_Parent_Queue($this->svc);
+    }
+
+    protected function exists(): bool
+    {
+        $this->read('?name=' . $this->name());
+        $this->entity = $this->read[0] ?? null;
+        $this->insertId = $this->read[0]['.id'] ?? null;
+        return (bool)$this->insertId;
+    }
+
+    protected function name()
+    {
+        return $this->svc->disabled
+            ? $this->conf->disabled_profile
+            : $this->svc->plan_name();
     }
 
 }
