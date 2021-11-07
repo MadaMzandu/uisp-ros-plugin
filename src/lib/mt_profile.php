5dcbfe54d828fad8757e1d3c21ff91ec65fa417b
Madalitso Mzandu <mada@datacom.mw>
2021-11-07 03:24:05 +0200

    optimised exists removed mt id functions

--- src/lib/mt_profile.php
+++ src/lib/mt_profile.php
@@ -9,15 +9,14 @@ class MT_Profile extends MT
     {
         parent::init();
         $this->path = '/ppp/profile/';
-        $this->findId();
-        $this->exists = (bool)$this->insertId;
+        $this->exists = $this->exists();
         $this->pq = new MT_Parent_Queue($this->svc);
     }
 
     public function set():bool
     {
         if ($this->svc->contention < 0 && !$this->has_children()) {
-            return $this->deleteProfile();
+            return $this->delete();
         }
         if (!$this->exists()) {
             return $this->insert();
@@ -82,7 +81,7 @@ class MT_Profile extends MT
             'rate-limit' => $this->rate()->text,
             'parent-queue' => $this->pq->name(),
             'address-list' => $this->conf->active_list,
-            '.id' => $this->name(),
+            '.id' => $this->insertId ?? $this->name(),
         ];
     }
 
@@ -120,13 +119,28 @@ class MT_Profile extends MT
     {
         $this->path = '/ppp/secret/';
         $read = $this->read('?profile=' . $this->name()) ?? [];
+        $disabled = $this->entity_disabled();
         $this->path = '/ppp/profile/';
-        $count = $read ? sizeof($read) : 0;
-        $count += $this->svc->disabled ? 0 : -1; // do not deduct if account is disabled
-        return $count ?: false;
+        $count = sizeof($read) ?? 0;
+        $count += $disabled ? 0 : -1; // do not deduct if account is disabled
+        return (bool) max($count,0);
     }
 
-    private function deleteProfile():bool
+    private function entity_disabled():bool
+    {
+        // $this->path = '/ppp/secret/'; path is already set by prev call
+        $read = $this->read('?comment');
+        $id = (string)$this->svc->id();
+        foreach($read as $item){
+            if(substr($item['comment'],0,strlen($id)) == $id){
+                return $item['profile'] == $this->conf->disabled_profile;
+            }
+        }
+        return false;
+    }
+
+
+    private function delete():bool
     {
         $this->svc->contention = -1;
         $data['.id'] = $this->data()->name;
