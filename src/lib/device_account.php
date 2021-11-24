58f19449c52129e586018dac5ff746123883314c
Madalitso Mzandu <mada@datacom.mw>
2021-11-24 05:51:40 +0200

    removed unused code

--- src/lib/device_account.php
+++ src/lib/device_account.php
@@ -5,14 +5,6 @@ include_once 'device_base.php';
 class Device_Account extends Device_Base
 {
 
-    protected $save; //data for save
-
-    protected function init(): void
-    {
-        parent::init();
-        $this->save = [];
-    }
-
     protected  function rate():stdClass
     {
         $rate = $this->svc->plan->rate();
