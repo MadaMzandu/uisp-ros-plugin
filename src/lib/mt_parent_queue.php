d307d1c8362043e334c78020aaa537f5bacc10ff
Madalitso Mzandu <mada@datacom.mw>
2021-12-07 14:22:44 +0200

    fixed parent first child

--- src/lib/mt_parent_queue.php
+++ src/lib/mt_parent_queue.php
@@ -63,7 +63,7 @@ class MT_Parent_Queue extends MT
     {
         return (object)array(
             'name' => $this->prefix() . '-child',
-            'packet-marks' => '1stchild',
+            'target' => '169.254.244.122',
             'parent' => $this->name(),
             'max-limit' => '1M/1M',
             'limit-at' => '1M/1M',
