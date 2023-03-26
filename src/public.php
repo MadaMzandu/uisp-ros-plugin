542b50c2423f63117f798b1d892cb97ecdbb88f6
Madalitso Mzandu <mada@datacom.mw>
2023-03-26 11:10:11 +0200

    removed unused code enabled cors

--- src/public.php
+++ src/public.php
@@ -1,6 +1,6 @@
 <?php
 chdir(__DIR__);
-//include_once 'includes/cors.php';
+include_once 'includes/cors.php';
 
 if (isset($_SERVER['REQUEST_METHOD'])
     && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') { //skip redirect for options
@@ -9,19 +9,11 @@ if (isset($_SERVER['REQUEST_METHOD'])
 
 require_once 'vendor/autoload.php';
 include_once 'lib/api_sqlite.php';
-include_once 'includes/api_setup.php';
 include_once 'lib/api_cache.php';
+include_once 'includes/api_setup.php';
 include_once 'lib/api_common.php';
+include_once 'lib/api_logger.php';
 
-//if(user_not_ok()){
-//    $status = [
-//        'status' => 'failed',
-//        'error' => true,
-//        'message' => 'User is not authenticated',
-//        'data' => []
-//    ];
-//    exit(json_encode($status));
-//}
 
 $json = file_get_contents('php://input') ?? null;
 
@@ -29,11 +21,13 @@ try
 {
     set_error_handler('myErrorHandler');
     run_setup();
+    MyLog()->Append('public: setup completed');
 
     include_once 'lib/api_router.php';
 
     if(!$json)
     {
+        MyLog()->Append('public: begin page load param '. json_encode($_GET ?? '{}'));
         if (isset($_GET['page']) && $_GET['page'] == 'panel')
         { //load panel
             include 'public/panel/index.php';
@@ -41,10 +35,13 @@ try
         exit();
     }
 
+    MyLog()->Append('public: begin api request: '.$json);
     $data = json_decode($json);
     $api = new API_Router($data);
     $api->route();
     $api->http_response();
+    MyLog()->Append('api: finished without error : '
+        . json_encode($api->status()));
 }
 catch (
 Exception
@@ -53,8 +50,9 @@ Exception
 | \Ubnt\UcrmPluginSdk\Exception\ConfigurationException
 | \Ubnt\UcrmPluginSdk\Exception\InvalidPluginRootPathException
 | \Ubnt\UcrmPluginSdk\Exception\JsonException $err){
-    MyLog()->appendLog('api: '.$err->getMessage().' request: '.$json);
+    MyLog()->Append('exeption triggered: '.$err->getMessage().' request: '.$json);
     respond($err->getMessage(),true);
 }
+MyLog()->Append('public: begin cache sync');
 
 run_cache($json);
