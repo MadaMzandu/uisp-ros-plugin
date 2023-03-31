4599b82d6632473af5683dae1960a2d679b6825e
Madalitso Mzandu <mada@datacom.mw>
2023-03-31 05:13:16 +0200

    changed to seconds

--- src/lib/api_timer.php
+++ src/lib/api_timer.php
@@ -7,10 +7,10 @@ class ApiTimer
     public function stop()
     {
         $in = $this->duration();
-        MyLog()->Append(sprintf('%s completed in %s milliseconds',$this->label,$in));
+        MyLog()->Append(sprintf('%s completed in %s seconds',$this->label,$in));
         return $in;
     }
-    private function duration(){ return (microtime(true) - $this->start) * 1000 ; }
-    public function start($label){$this->label = $label; $this->start = microtime(true); }
+    private function duration(){ return (microtime(true) - $this->start) ; }
+    public function start($label = 'task'){$this->label = $label; $this->start = microtime(true); }
     public function __construct($label = 'task'){ $this->start(); $this->label = $label; }
 }
