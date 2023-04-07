42f2b82c116f436a43d9595fe56900f0fb06332b
Madalitso Mzandu <mada@datacom.mw>
2023-04-07 09:49:54 +0200

    Update api_attributes.php

--- src/lib/api_attributes.php
+++ src/lib/api_attributes.php
@@ -5,17 +5,13 @@ include_once 'api_ucrm.php';
 
 class ApiAttributes
 {
-    private $cid = 0 ;
-    private $uid ;
-    private $pid ;
 
     public function check($request): bool
     {
-        $this->check_config();
+        if(!$this->check_config()) return false ;
         $entity = $request->extraData->entity ?? $request ;
         $attributes = $entity->attributes ?? null ;
-        $this->cid = $entity->clientId ;
-        $values = $this->map_attributes($attributes);
+        $values = $this->extract($attributes);
         if(empty($values)) return false ;
         if(!$this->check_device($values)) return false ;
         if($this->check_mac($values)) return true ;
@@ -25,36 +21,36 @@ class ApiAttributes
     private function check_username($values):bool
     {
         $user = $values['username'] ?? null ;
-        if($user) return true ;
+        return (bool) $user ;
+    }
+
+    private function auto_user($values): bool
+    {
         $conf = $this->conf();
         $hotspot = $values['hotspot'] ?? false ;
-        $auto = $conf->auto_ppp_user || ($hotspot && $conf->auto_hs_user);
-        if(!$auto) return false ;
-        $this->set_username();
-        return false ;
+        return $conf->auto_ppp_user || ($hotspot && $conf->auto_hs_user);
     }
 
-    public function set_username(): void
+    public function set_username($cid): void
     {
-        $user = ['customAttributeId' => $this->uid,
-            'value' => sprintf('%s-%s',$this->string(5),$this->cid)] ;
-        $pass = ['customAttributeId' => $this->pid, 'value' => $this->string()];
-        $post = ['attributes' => [$user,$pass]];
-        $this->ucrm()->patch('clients/'.$this->cid,$post);
+        $ids = $this->attribute_id_map();
+        $uid = $ids['pppoe_user_attr'] ?? null ;
+        $pid = $ids['pppoe_pass_attr'] ?? null ;
+        $post = [];
+        if($uid)
+        $post['attributes'][] = ['customAttributeId' => $uid, 'value' =>
+            sprintf('%s-%s',$this->string(5),$cid)] ;
+        if($pid) $post['attributes'][] = ['customAttributeId' => $pid, 'value' => $this->string()];
+        if($post) $this->ucrm()->patch('clients/'.$cid,$post);
     }
 
-    private function check_config()
+    public function check_config(): bool
     {
-        $user_key = $this->conf()->pppoe_user_attr ?? null ;
-        $pass_key = $this->conf()->pppoe_pass_attr ?? null ;
-        if(!($user_key && $pass_key)){
-            throw new Exception('attributes: plugin not ready - missing custom attributes');
-        }
-        $attrs = $this->ucrm()->get('custom-attributes',['attributeType' => 'service']);
-        foreach ($attrs as $attr){
-            if($attr->key == $user_key) $this->uid = $attr->id ;
-            if($attr->key == $pass_key) $this->pid = $attr->id ;
-        }
+        $assigned = $this->assigned_map();
+        $device = $assigned['device_name_attr'] ?? null ;
+        $user = $assigned['pppoe_user_attr'] ?? null ;
+        $mac = $assigned['mac_addr_attr'] ?? null ;
+        return $device && ($user || $mac);
     }
 
     private function string($length = 8): string
@@ -91,26 +87,73 @@ class ApiAttributes
         return (bool) $device ;
     }
 
-    private function map_attributes($attributes): ?array
-    {
-        if(empty($attributes)) return null ;
-        $native_map = $this->native_map() ;
-        $conf = $this->conf();
+    public function extract($attributes): array
+    {//extract attribute values and map to database keys
+        if(empty($attributes)){ return [] ;}
+        $assigned = array_flip($this->assigned_map());
+        $native = $this->native_map();
+        $devices = $this->device_map();
         $map = [];
-        foreach ($attributes as $attribute){
-            foreach (array_keys($native_map) as $native_key){
-                $assigned = $conf->$native_key ?? null;
-                if($assigned && $assigned == $attribute->key){
-                    $key = $native_map[$native_key];
-                    $map[$key] = $attribute->value ;
-                }
+        foreach($attributes as $attribute){
+            $key = $assigned[$attribute->key] ?? null ;
+            $db_key = $native[$key] ?? null ;
+            if($db_key){ $map[$db_key] = $attribute->value ?? null; }
+        }
+        $device = strtolower($map['device']) ?? null ;
+        $map['device'] = $devices[$device] ?? null ;
+        return $map ;
+    }
+
+    private function device_map()
+    {
+        $devices = $this->db()->selectAllFromTable('devices');
+        $map = [];
+        foreach ($devices as $device){
+            $name = strtolower($device['name']);
+            $map[$name] = $device['id'];
+        }
+        return $map ;
+    }
+
+    private function attribute_id_map(): array
+    {// native key to uisp id
+        $assigned = $this->assigned_map();
+        $attributes = $this->attribute_map() ;
+        $map = [];
+        foreach(array_keys($assigned) as $native_key){
+            $key = $assigned[$native_key] ?? null ;
+            if($key){
+                $attribute = $attributes[$key] ?? null ;
+                if($attribute){$map[$native_key] = $attribute->id ; }
             }
         }
         return $map ;
     }
 
+    private function attribute_map(): array
+    { //key to attribute map
+        $read = $this->ucrm()->get('custom-attributes') ?? [];
+        $map = [];
+        foreach ($read as $item){
+            $map[$item->key] = $item ;
+        }
+        return $map ;
+    }
+
+    private function assigned_map(): array
+    {// native key to assigned key map
+        $conf = $this->conf();
+        $native_keys = array_keys($this->native_map());
+        $map = [];
+        foreach ($native_keys as $native_key){
+            $assigned = $conf->$native_key ?? null ;
+           if($assigned) $map[$native_key] = $assigned;
+        }
+        return $map ;
+    }
+
     private function native_map(): array
-    {
+    { // plugin native key to database key map
         return [
             'device_name_attr' => 'device',
             'pppoe_user_attr' => 'username',
