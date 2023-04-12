d96a92295e83df2446f03d81bce625545316772c
Madalitso Mzandu <mada@datacom.mw>
2023-04-12 22:24:24 +0200

    disable listing

--- src/lib/api_jobs.php
+++ src/lib/api_jobs.php
@@ -16,7 +16,8 @@ class Api_Jobs extends Admin
 
     public function list()
     {
-        $this->result = $this->queue ?? [];
+        $this->result = [];
+//        $this->result = $this->queue ?? [];
     }
 
     public function clear()
