cd6693f319e9c85ff8e667b2577b302a10c4f3bf
Madalitso Mzandu <mada@datacom.mw>
2023-09-09 15:14:59 +0200

    do not delete after merge

--- src/lib/admin_plans.php
+++ src/lib/admin_plans.php
@@ -10,51 +10,48 @@ class AdminPlans extends Admin
 
     private function merge()
     {
-        $defaults = ['priorityUpload' => 8,'priorityDownload' => 8,'timeUpload' => 1,'timeDownload' => 1];
         $plans = $this->get_plans();
         $config = $this->get_config() ;
-        $keys = ['ratio','priorityUpload','priorityDownload','limitUpload','limitDownload',
-            'burstUpload','burstDownload','threshUpload','threshDownload','timeUpload','timeDownload'];
         foreach($plans as $plan){
-            $id = $plan['id'] ;
-            foreach($keys as $key){
-                $plans[$id][$key] = $config[$id][$key]
-                    ?? $defaults[$key] ?? 0;
-            }
+            $saved = $config[$plan['id']] ?? [] ;
+            $config[$plan['id']] = array_replace($saved,$plan);
         }
-        return $plans ;
+        return $config ;
     }
 
-    private function update(): array
+    private function defaults(): array
+    {
+        $keys = ['ratio','priorityUpload','priorityDownload','limitUpload','limitDownload',
+            'burstUpload','burstDownload','threshUpload','threshDownload','timeUpload','timeDownload',];
+        $fill = array_fill_keys($keys,0);
+        $fill['priorityUpload'] = 8 ;
+        $fill['priorityDownload'] = 8 ;
+        return $fill ;
+    }
+
+    public function update(): array
     {
         $plans = $this->merge();
-        $ids = array_keys($plans);
-        $delete_expired = sprintf('DELETE FROM plans WHERE id NOT IN (%s)',implode(',',$ids)) ;
-        $this->db()->exec($delete_expired);
-        $keys = "id,ratio,priorityUpload,priorityDownload,limitUpload,limitDownload,burstUpload,".
-        "burstDownload,threshUpload,threshDownload,timeUpload,timeDownload";
-        $update = sprintf("INSERT OR IGNORE INTO plans (%s) VALUES ",$keys);
-        $values = [];
+        $update = [];
         foreach ($plans as $plan){
-            $row = [];
-            foreach(explode(',',$keys) as $key) {
-                $row[] = $plan[$key] ?? 0 ;
-            }
-            $values[] = sprintf("(%s)",implode(',',$row));
+            $update[] = array_diff_key($plan,['uploadSpeed'=>0,'downloadSpeed' => 0]);
         }
-        $update .= implode(',',$values);
-        $this->db()->exec($update);
+        $this->db()->insert($update,'plans',true);
         return $plans;
     }
 
     private function get_plans(): array
     {
-        $read = $this->ucrm()->get('service-plans');
+        $data = $this->ucrm()->get('service-plans');
+        $read = json_decode(json_encode($data),true);
         $tmp = [];
+        $trimmer = null ;
         foreach ($read as $item) {
-            $tmp[$item->id] = $item ;
+            $trimmer ??= array_diff_key($item,['id' => 0,'uploadSpeed' => 0,'downloadSpeed' => 0]);
+            $trim = array_diff_key($item,$trimmer);
+            $tmp[$item['id']] = array_replace($this->defaults(),$trim);
         }
-        return json_decode(json_encode($tmp),true) ;
+        return $tmp ;
     }
 
     private function get_config(): array
