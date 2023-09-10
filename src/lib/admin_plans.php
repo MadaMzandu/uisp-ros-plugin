86b0e98bd19f82d799370d8e034087d2a5ca701a
Madalitso Mzandu <mada@datacom.mw>
2023-09-10 06:29:32 +0200

    fixed speed

--- src/lib/admin_plans.php
+++ src/lib/admin_plans.php
@@ -21,8 +21,9 @@ class AdminPlans extends Admin
 
     private function defaults(): array
     {
-        $keys = ['ratio','priorityUpload','priorityDownload','limitUpload','limitDownload',
-            'burstUpload','burstDownload','threshUpload','threshDownload','timeUpload','timeDownload',];
+        $keys = ['ratio','uploadSpeed','downloadSpeed','priorityUpload','priorityDownload',
+            'limitUpload','limitDownload', 'burstUpload','burstDownload','threshUpload',
+            'threshDownload','timeUpload','timeDownload',];
         $fill = array_fill_keys($keys,0);
         $fill['priorityUpload'] = 8 ;
         $fill['priorityDownload'] = 8 ;
@@ -34,8 +35,8 @@ class AdminPlans extends Admin
         $plans = $this->merge();
         $update = [];
         foreach ($plans as $plan){
-            $update[] = array_diff_key($plan,['uploadSpeed'=>0,
-                'downloadSpeed' => 0,'last' => -1,'created' => -1]);
+            $update[] = array_diff_key($plan,['name'=> null,
+                'last' => -1,'created' => -1]);
         }
         $this->db()->insert($update,'plans',true);
         return $plans;
@@ -43,12 +44,12 @@ class AdminPlans extends Admin
 
     private function get_plans(): array
     {
-        $data = $this->ucrm()->get('service-plans');
+        $data = $this->ucrm()->get('service-plans',['public' => 0]);
         $read = json_decode(json_encode($data),true);
         $tmp = [];
         $trimmer = null ;
         foreach ($read as $item) {
-            $trimmer ??= array_diff_key($item,['id' => 0,'uploadSpeed' => 0,'downloadSpeed' => 0]);
+            $trimmer ??= array_diff_key($item,['id' => 0,'uploadSpeed' => 0,'downloadSpeed' => 0,'name' => null]);
             $trim = array_diff_key($item,$trimmer);
             $tmp[$item['id']] = array_replace($this->defaults(),$trim);
         }
