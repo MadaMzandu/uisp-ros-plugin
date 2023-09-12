ac4dd43994cf9c4fce1d1284d37289940f51bb9b
Madalitso Mzandu <mada@datacom.mw>
2023-09-12 12:20:16 +0200

    added sites

--- src/includes/cache.sql
+++ src/includes/cache.sql
@@ -1,37 +1,39 @@
+BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "clients" (
-       "id"    INTEGER NOT NULL,
-       "company" TEXT COLLATE NOCASE,
-       "firstName" TEXT COLLATE NOCASE,
-       "lastName" TEXT COLLATE NOCASE,
-       PRIMARY KEY("id")
-);
+     "id"	INTEGER NOT NULL,
+     "company"	TEXT COLLATE NOCASE,
+     "firstName"	TEXT COLLATE NOCASE,
+     "lastName"	TEXT COLLATE NOCASE,
+     PRIMARY KEY("id"));
 CREATE TABLE IF NOT EXISTS "network" (
-         "id"    INTEGER NOT NULL,
-         "address" TEXT ,
-         "address6" TEXT,
-         PRIMARY KEY("id")
-);
+     "id"	INTEGER NOT NULL,
+     "address"	TEXT,
+     "address6"	TEXT,
+     PRIMARY KEY("id"));
 CREATE TABLE IF NOT EXISTS "services" (
-       "id"    INTEGER NOT NULL,
-       "clientId" INTEGER,
-       "planId" INTEGER,
-       "device" INTEGER ,
-       "status" INTEGER ,
-       "uploadSpeed" REAL,
-       "downloadSpeed" REAL,
-       "username" TEXT COLLATE NOCASE,
-       "password" TEXT,
-       "mac" TEXT COLLATE NOCASE,
-       "duid" TEXT COLLATE NOCASE,
-       "iaid" TEXT COLLATE NOCASE,
-       "hotspot" INTEGER,
-       "price" REAL ,
-       "totalPrice" REAL ,
-       "currencyCode" TEXT ,
-       "callerId" TEXT,
-       PRIMARY KEY("id")
-);
-
+      "id"	INTEGER NOT NULL,
+      "clientId"	INTEGER,
+      "planId"	INTEGER,
+      "device"	INTEGER,
+      "status"	INTEGER,
+      "uploadSpeed"	REAL,
+      "downloadSpeed"	REAL,
+      "username"	TEXT COLLATE NOCASE,
+      "password"	TEXT,
+      "mac"	TEXT COLLATE NOCASE,
+      "duid"	TEXT COLLATE NOCASE,
+      "iaid"	TEXT COLLATE NOCASE,
+      "hotspot"	INTEGER,
+      "price"	REAL,
+      "totalPrice"	REAL,
+      "currencyCode"	TEXT,
+      "callerId"	TEXT,
+      PRIMARY KEY("id"));
+CREATE TABLE IF NOT EXISTS "sites" (
+       "id"	TEXT,
+       "service"	INTEGER,
+       "devices" INTEGER DEFAULT 0,
+       PRIMARY KEY("id"));
 CREATE INDEX IF NOT EXISTS "client_index" ON "services" ("clientId");
 CREATE INDEX IF NOT EXISTS "plan_index" ON "services" ("planId");
 CREATE INDEX IF NOT EXISTS "status_index" ON "services" ("status");
@@ -40,3 +42,5 @@ CREATE INDEX IF NOT EXISTS "user_index" ON "services" ("username");
 CREATE INDEX IF NOT EXISTS "mac_index" ON "services" ("mac");
 CREATE INDEX IF NOT EXISTS "address_index" ON "network" ("address");
 CREATE INDEX IF NOT EXISTS "address6_index" ON "network" ("address6");
+CREATE INDEX IF NOT EXISTS "site_index" ON "sites" ("service");
+COMMIT;
