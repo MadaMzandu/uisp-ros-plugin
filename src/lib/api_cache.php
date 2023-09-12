5b65bc7acd4db1b0f7e6eaa7e2c9c4ee4aa23da7
Madalitso Mzandu <mada@datacom.mw>
2023-09-12 19:03:59 +0200

    added device value

--- src/lib/api_cache.php
+++ src/lib/api_cache.php
@@ -36,7 +36,7 @@ class ApiCache{
             }
             $timer = new ApiTimer('sync: ');
             MyLog()->Append('populating services,clients');
-            foreach(['clients','services'] as $table){
+            foreach(['clients','services','sites'] as $table){
                 $this->populate($table);
                 MyLog()->Append('finished populating: '.$table);
             }
