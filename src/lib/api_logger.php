50304b3d640b43576273dae53a72b35a6dba78be
Madalitso Mzandu <mada@datacom.mw>
2024-01-16 11:19:02 +0200

    updated debug

--- src/lib/api_logger.php
+++ src/lib/api_logger.php
@@ -1,40 +1,41 @@
 <?php
-require_once  'vendor/autoload.php';
 
-const API_DEBUG_LEVEL = 6;
+const API_DEBUG_ECHO = 0;
 
 class ApiLogger
 {
     public function Append($entry, $level = LOG_DEBUG)
     {
         if (!$this->Debug() && $level >= LOG_DEBUG){ return; }
-        if(is_array($entry)){ $entry = json_encode($entry); }
-        error_log(sprintf("%s: %s",$this->Time(), $entry) . PHP_EOL,3,'data/plugin.log');
-//      echo sprintf("%s: %s",$this->Time(), $entry) . PHP_EOL;
-//       $this->ULog()->appendLog(sprintf("%s: %s",$this->Time(), $entry));
+        $fn = 'data/plugin.log';
+        if(is_array($entry) || is_object($entry)){ $entry = json_encode($entry); }
+        error_log(sprintf("%s: %s",$this->Time(), $entry) . PHP_EOL,3,$fn);
+        if(API_DEBUG_ECHO) echo sprintf("%s: %s",$this->Time(), $entry) . PHP_EOL;
     }
 
     private function Debug(): bool
     {
         $fn = 'data/config.json';
-        if(!is_file($fn)){ return '0'; }
-        $file = file_get_contents($fn) ?? '{}';
-        $conf = json_decode($file);
-        return $conf->debugEnable ?? '0';
-    }
-
-    private function ULog()
-    {
-        $l = \Ubnt\UcrmPluginSdk\Service\PluginLogManager::create();
-        return $l;
+        if(!is_file($fn)){ return false ;}
+        $conf = json_decode(file_get_contents($fn));
+        if(is_object($conf)){
+            return $conf->debugEnable ?? false ;
+        }
+        return false;
     }
 
     private function Time()
     {
-        return (new DateTime())->format('Y-m-d H:i:s.v');
+        return date('Y-m-d H:i:s.v');
     }
 }
 
+$apiLogger = null ;
+
 function MyLog(){
-    return new ApiLogger();
+    global $apiLogger;
+    if(empty($apiLogger)){
+        $apiLogger = new ApiLogger();
+    }
+    return $apiLogger ;
 }
