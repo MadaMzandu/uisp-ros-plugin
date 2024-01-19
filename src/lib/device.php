1b87ab40f41bb89e2c801497a0cf7bc38de461f1
Madalitso Mzandu <mada@datacom.mw>
2024-01-19 06:27:37 +0200

    removed device fix

--- src/lib/device.php
+++ src/lib/device.php
@@ -1,9 +1,9 @@
 <?php
-include_once 'device_fix.php';
+include_once 'device_account.php';
 
 // extend new device types from here
 
-class Device extends Device_Fix
+class Device extends Device_Account
 {
 
 
