3bdc254431a5c70834fda124e6bf15a56e1f1975
Madalitso Mzandu <mada@datacom.mw>
2024-01-20 07:01:11 +0200

    enabled cors

--- src/public.php
+++ src/public.php
@@ -2,7 +2,7 @@
 
 
 chdir(__DIR__);
-include_once 'includes/cors.php';
+//include_once 'includes/cors.php';
 require_once 'vendor/autoload.php';
 include_once 'lib/api_logger.php';
 include_once 'lib/api_common.php';
@@ -35,10 +35,8 @@ try
         exit();
     }
 
-
     if(!$json)
     {
-        MyLog()->Append('public: begin page load param '. json_encode($_GET ?? '{}'));
         if (isset($_GET['page']) && $_GET['page'] == 'panel')
         { //load panel
             include 'public/panel/index.php';
@@ -46,13 +44,10 @@ try
         exit();
     }
 
-
-//    MyLog()->Append('public: begin api request: '.$json);
     $data = json_decode($json);
     $api = new API_Router($data);
     $api->route();
     $api->http_response();
-    MyLog()->Append('api: finished without error : ' . json_encode($api->status()));
 }
 catch ( \Exception | Error $error)
 {
