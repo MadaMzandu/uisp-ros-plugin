fe8dacf4596d5954566ea33316b8360815b22137
Madalitso Mzandu <mada@datacom.mw>
2024-01-21 05:52:00 +0200

    removed redundant parameter

--- src/lib/api_system.php
+++ src/lib/api_system.php
@@ -55,7 +55,7 @@ class ApiSystem extends Admin
             $trim['path'] = $item['path'];
             $batch[] = $trim ;
             if($sid){
-                mySqlite()->delete($sid,'services');
+                mySqlite()->delete($sid);
                 mySqlite()->delete($sid,'network');
             }
         }
