57be6d6204ec052824ee04670a16a03e37d02509
Madalitso Mzandu <mada@datacom.mw>
2024-01-22 15:42:28 +0200

    completed service and config listing

--- src/lib/api_list.php
+++ src/lib/api_list.php
@@ -7,12 +7,20 @@ class ApiList
     private null|object|array $result = null ;
     private null|object|array $data;
 
-    public function list(): null|array
+    public function list(): null|array|object
     {
         return match ($this->mode){
             'plans' => $this->list_plans(),
+            'devices' => $this->list_devices(),
+            'services' => $this->list_services(),
+            'config' => $this->list_config(),
             default => null
         };
+    }
+
+    private function list_config(): null|array|object
+    {
+        return $this->db()->readConfig() ;
     }
 
     private function list_plans(): array
@@ -42,6 +50,90 @@ class ApiList
         return $from_db ;
     }
 
+    private function list_devices(): array
+    {
+        $devices = $this->find_devices() ;
+        $count = $this->count_device_users() ;
+        $status = $this->check_status($devices) ;
+        $disabled = $this->find_disabled();
+        $list = [];
+        foreach($devices as $device){
+            $device['users'] = $count[$device['id']] ?? 0;
+            $device['status'] = $status[$device['id']] ?? false;
+            $device['disabled'] = $disabled[$device['id']] ?? false ;
+            MyLog()->Append(["DEVICE",$device]);
+            $list[] = $device ;
+        }
+        return $list ;
+    }
+
+    private function find_disabled(): array
+    {
+        $conf = $this->db()->readConfig()->disabled_routers ?? null;
+        $disabled = [];
+        if($conf) foreach (explode(',',$conf) as $id){
+            $disabled[$id] = true; }
+        return $disabled ;
+    }
+
+    private function check_status($devices): array
+    {
+        $ports = ['mikrotik' => 8291,'edgos' => 443,
+            'cisco' => 22,'radius' => 3301];
+        $status = [];
+        foreach ($devices as $device) {
+            $id = $device['id'];
+            $type = $device['type'] ?? 'mikrotik';
+            $port = $device['port'] ?? $ports[$type];
+            try{
+                $conn = @fsockopen($device['ip'],
+                    $port,
+                    $code, $err, 3);
+                $state = is_resource($conn);
+                $status[$id] = $state ;
+                if($state){ fclose($conn); }
+            }
+            catch (\Exception $err){
+                $status[$id] = false ;
+            }
+        }
+        return $status ;
+    }
+
+    private function count_device_users(): array
+    {
+        $q = 'select device,count(id) as count from services '.
+            'where status not in (2,5,8) group by device';
+        $r = $this->db()->selectCustom($q) ;
+        $count = [];
+        foreach($r as $item){ $count[$item['device']] = $item['count']; }
+        return $count ;
+    }
+
+    private function list_services(): array
+    {
+        $db = $this->dbx2();
+        $data = ['data' => []];
+        $data['count'] = $db->querySingle($this->svc_sql(true)) ?? 0;
+        $f = $db->query($this->svc_sql());
+        while($r = $f->fetchArray(SQLITE3_ASSOC)){
+            $r['address'] ??= $r['a4'] ?? null;
+            $r['address6'] ??= $r['a6'] ?? null;
+            $data['data'][] = $r ;
+        }
+        MyLog()->Append(['list_services','items: '. sizeof($data['data'])]);
+        return $data ;
+
+    }
+
+    private function find_devices(): array
+    {
+        $r = $this->db()->selectAllFromTable('devices');
+        $tmp = [];
+        foreach ($r as $item) { $tmp[$item['id']] = $item ; }
+        return $tmp;
+    }
+
     private function find_db_plans(): array
     {
         $read = $this->db()->selectAllFromTable('plans');
@@ -50,7 +142,7 @@ class ApiList
         return $tmp ;
     }
 
-    public function find_plans(): array
+    private function find_plans(): array
     {
         $data = $this->ucrm()->get('service-plans',['servicePlanType' => 'internet']);
         $tmp = [];
@@ -63,7 +155,6 @@ class ApiList
         return $tmp ;
     }
 
-
     private function set_mode($mode)
     {
         if(preg_match("#(service|device|plan)#",$mode)){ //append ending "s"
@@ -72,10 +163,55 @@ class ApiList
         $this->mode = $mode ;
     }
 
+    private function svc_sql($count = false): string
+    {
+        $fields = "services.*,services.username,services.mac,services.price,".
+            "main.network.address,main.network.address6,clients.company,clients.firstName,".
+            "clients.lastName,plans.name as plan,cache.network.address as a4,".
+            "cache.network.address6 as a6";
+        if($count){ $fields = "count(main.services.id)"; }
+        $did = $this->data->data->did ?? $this->data->data->id ?? $this->data->data->device ?? 0;
+        $sql = "SELECT $fields FROM main.services ".
+            "LEFT JOIN cache.clients ON main.services.clientId=clients.id ".
+            "LEFT JOIN main.network ON main.services.id=main.network.id ".
+            "LEFT JOIN cache.network on main.services.id=cache.network.id ".
+            "LEFT JOIN main.plans on main.services.planId=plans.id ".
+            "LEFT JOIN cache.services ON main.services.id=cache.services.id ".
+            "WHERE main.services.device = $did AND main.services.status NOT IN (2,5,8) ";
+        $query = $this->data->data->query ?? null ;
+        if($query){
+            if(is_numeric($query)){
+                $sql .= "AND (main.services.id=$query OR main.services.clientId=$query) ";
+            }
+            else{
+                $sql .= "AND (clients.firstName LIKE '%$query%' OR clients.lastName LIKE '%$query%' ".
+                    "OR clients.company LIKE '%$query%' OR services.username LIKE '%$query%' OR ".
+                    "services.mac LIKE '%$query%') ";
+            }
+        }
+        $limit = $this->data->limit ?? 100 ;
+        $offset = $this->data->offset ?? 0 ;
+        if(!$count) { $sql .=  "ORDER BY main.services.id LIMIT $limit OFFSET $offset "; }
+        return $sql;
+    }
+
+    private function dbx2()
+    {
+        $fn = 'data/data.db';
+        $fn2 = 'data/cache.db';
+        $db = new SQLite3($fn);
+        $db->exec("ATTACH '$fn2' as cache");
+        return $db ;
+    }
+
     private function db(): ApiSqlite { return mySqlite(); }
+
     private function cachedb(): ApiSqlite { return myCache(); }
-    private function status(): object { return new stdClass(); }
-    private function result(): null|array|object { return $this->result; }
+
+    public function status(): object { return new stdClass(); }
+
+    public function result(): null|array|object { return $this->result; }
+
     private function ucrm($assoc = true): ApiUcrm { return new ApiUcrm(null,$assoc); }
 
     public function __construct($data = null,$mode = 'services')
