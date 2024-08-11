416f4188a9a343354d066a8f738c381a883cc781
Madalitso Mzandu <mada@datacom.mw>
2024-08-11 05:27:24 +0200

    curl default timeout to 10 seconds

--- src/lib/api_curl.php
+++ src/lib/api_curl.php
@@ -57,7 +57,7 @@ class ApiCurl
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 10,
             CURLOPT_VERBOSE => $this->verbose,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
