6b34df4c361b92cbaa149b945fa65cb85e15eabb
Madalitso Mzandu <mada@datacom.mw>
2025-01-24 12:04:33 +0200

    added ip management

---
+++ src/lib/api_ip_mgmt.php
@@ -0,0 +1,60 @@
+<?php
+include_once "api_ucrm.php";
+include_once "api_sqlite.php";
+
+function find_attr($nk): ?array
+{
+    $api = new ApiSqlite();
+    $c = $api->readConfig() ;
+    $k = $c->$nk ?? null ; //native key to user key
+    if(!$k){ return null; }
+    $a = get_attrs() ;
+    return $a[$k] ?? null ;
+}
+
+function get_attrs(): array
+{
+    $api = new ApiUcrm();
+    $api->assoc = true ;
+    $r = $api->get('custom-attributes');
+    $m = [];
+    foreach($r as $a){ //map by key
+        $m[$a['key']] = $a ;
+    }
+    return $m;
+}
+
+function get_ips(): array
+{
+    $api = new ApiSqlite();
+    $s = "SELECT id,address from network";
+    $r = $api->selectCustom($s) ;
+    $m = [];
+    foreach ($r as $i){
+        $m[$i['id']] = $i['address'] ;
+    }
+    return $m;
+}
+
+$fn = 'data/config.json';
+$config = is_file($fn) ? json_decode(file_get_contents($fn),true) : [];
+$config['ipUpdateHour'] ??= 1;
+$hour = $config['ipUpdateHour'] ?? -1;
+$date = $config['ipUpdateDate'] ?? '2025-01-01';
+$now = date('Y-m-d');
+$curr = (int) date('G');
+if($hour != $curr || $date == $now){
+    //return  ;
+}
+
+$attr = find_attr('ip_addr_attr') ;
+if(!$attr){ return ; }
+$attrid = $attr['id'] ;
+$ips = get_ips() ;
+$api = new ApiUcrm() ;
+foreach($ips as $id => $addr){
+    $ud = [['customAttributeId' => $attrid,'value' => $addr]];
+    $api->patch("clients/services/$id",['attributes' => $ud]);
+}
+$config['ipUpdateDate'] = $now;
+file_put_contents($fn,json_encode($config,128));
--- src/main.php
+++ src/main.php
@@ -3,3 +3,5 @@ require_once __DIR__ . '/lib/api_jobs.php';
 $data = [];
 $jobs = new ApiJobs($data);
 $jobs->run();
+
+include_once "lib/api_ip_mgmt.php";
